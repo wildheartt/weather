0700af3294d359d080f8baff9ca933a136dd2a33
Your Name <wildheartss@tutamail.com>
2025-02-01 13:15:00 +0300

    fixing eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import importPlugin from 'eslint-plugin-import';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
